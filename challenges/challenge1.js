f744b49381e650d8c4a9520fb623f865c015e9c9
sandycamilo <39415553+sandycamilo@users.noreply.github.com>
2021-02-01 21:38:57 -0800

    finished challeges 1 and 2

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -4,16 +4,19 @@
  * Follow the steps below and answer the discusssion questions that follow.
  * 
  * 1. Read over the code that follows. In what order will the outputs "Step 1",
- *    "Step 2", and "Step 3" be printed? How do you know?
+ *    "Step 2", and "Step 3" be printed? How do you know? 1, 3, 2 - step 2 is printed when function snooze is called after step 1 is printed. 
  * 
  * 
  * 2. Run this code using `node challenge1.js`. In what order were the steps
- *    printed?
+ *    printed?  Step 1
+                Step 3
+                Step 2
+                Async Action completed via callback
  * 
  * 
  * 3. Change the delay time in the `snooze` function from 2000 ms to 0. In what
  *    order will the steps be printed now? Why? Re-run the code again to verify
- *    your expectation. Were you correct?
+ *    your expectation. Were you correct? It will stay the same because console.log - step 1 - is printed before the snooze function is called. 
  * 
  *******************************************************************************
  */
@@ -22,7 +25,7 @@
 function snooze(action) {
     setTimeout(function() {
       action();
-    }, 2000);
+    }, 0);
 }
 console.log('Step 1');
 
--- challenges/challenge2.js
+++ challenges/challenge2.js
@@ -5,16 +5,25 @@
  * 
  * 1. Read over the code that follows. What will be printed to the console when
  *    it runs? Run the code using `node challenge2.js` and verify that your
- *    expectation was correct.
+ *    expectation was correct. 'Hello there, Ducky' and 'MAKE SCHOOL IS AWESOME!!!'
  * 
  * 
  * 2. What happens if greet() fails? Make it fail by changing 'name' to a number
- *    instead of a string. What happens? Does uppercaser() still run?
+ *    instead of a string. What happens? Does uppercaser() still run? If we add an int instead of
+ *    a string and run greet it will print console.log('Received an error!') since it catches the error. 
+ *    It then prints 'Name must be a string!' when the function runs and identifies that name does not equal to a string. 
+ *    Uppercase() does not run because it does not print the console log  the greet result. The uppercase() function 
+ *    is only executed when the greet() function logs the greeting. 
+ * 
  * 
  * 
  * 3. What happens if greet() succeeds and uppercaser() fails? Modify your code
  *    to achieve this result by changing the values of 'name' and 'my_str' and
- *    run the code again.
+ *    run the code again. We get this as an output: 
+ *     Hello there, Make School is Awesome!!! - We add a string so it returns the greeting + the new string.
+       Received an error! - When the function is called it detects the error and logs it.
+       Argument to uppercaser must be string - We change string to int and when uppercase() function runs, we get an output stating 'Argument to uppercaser must be string' because 
+       'type of str'  does not equal to a string.
  * 
  * 
  * 4. Write a method that takes a string as input and returns the input string
@@ -63,8 +72,27 @@ function uppercaser(str) {
     });
 }
 
-name = 'Ducky'
+function spacer(str) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      if (typeof str === 'string') { 
+        newString = ""
+        for (letter of str) {
+          newString += letter + " "
+        }
+          resolve(newString);
+      } else {
+          reject('Name must be a string!');
+      }
+    }, 1600);
+  });
+}
+
+
+name = 'kau'
 my_str = 'Make School is Awesome!!!'
+spacename = 'flower'
+
 
 greet(name)
     .then((greetResult) => {
@@ -73,7 +101,12 @@ greet(name)
     })
     .then((uppercaserResult) => {
         console.log(uppercaserResult)
+        return spacer(spacename);
+    })
+    .then((spacerResult) => {
+      console.log(spacerResult)
     }).catch((err) => {
         console.log('Received an error!')
         console.log(err);
     });
+
